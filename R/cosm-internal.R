44dcd6976c24de8c650892def5e1b965ab3ec241
David Holstius <david.holstius@gmail.com>
2012-06-27 09:34:10 -0700

    refactor S3 class tidbits

--- R/cosm-internal.R
+++ R/cosm-internal.R
@@ -4,3 +4,24 @@ update.list <- function(object, ...) {
 		object[[key]] <- args[[key]]
 	return(object)
 }
+
+parse.named <- function(pattern, x) {
+	tryCatch({
+		matches <- regexpr(pattern, x, perl=TRUE)
+		f <- function(i) {
+			if(matches[i] == -1) return("")
+			st <- attr(matches, "capture.start")[i, ]
+			substring(x[i], st, st + attr(matches, "capture.length")[i, ] - 1)
+		}
+		m <- do.call(rbind, lapply(seq_along(matches), f))
+		if (is.null(m)) {
+			return(NULL)
+		} else {
+			colnames(m) <- attr(matches, "capture.names")
+			return(m)
+		}
+	}, error = function(e) {
+		warning("Sorry, couldn't match pattern ", pattern, " to ", x)
+		stop(e)
+	})
+}
